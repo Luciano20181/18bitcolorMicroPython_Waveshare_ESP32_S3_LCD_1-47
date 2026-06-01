f68d12d2f94a987649057cabf7ed393d91cd92ce
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-06-01 11:10:48 -0300

    Update tjpgd565.h

--- jd9853/jpg/tjpgd565.h
+++ jd9853/jpg/tjpgd565.h
@@ -3,13 +3,14 @@
 /----------------------------------------------------------------------------*/
 #ifndef DEF_TJPGDEC
 #define DEF_TJPGDEC
+
 /*---------------------------------------------------------------------------*/
 /* System Configurations */
 
-#define	JD_SZBUF		512	/* Size of stream input buffer */
-#define JD_FORMAT		1	/* Output pixel format 0:RGB888 (3 BYTE/pix), 1:RGB565 (1 WORD/pix) */
-#define	JD_USE_SCALE	1	/* Use descaling feature for output */
-#define JD_TBLCLIP		1	/* Use table for saturation (might be a bit faster but increases 1K bytes of code size) */
+#define	JD_SZBUF		512		/* Size of stream input buffer */
+#define JD_FORMAT		0		/* 0: RGB888 (3 bytes/pixel), 1: RGB565 (2 bytes/pixel) */
+#define	JD_USE_SCALE	1		/* Use descaling feature for output */
+#define JD_TBLCLIP		1		/* Use table for saturation */
 
 /*---------------------------------------------------------------------------*/
 
@@ -19,60 +20,39 @@ extern "C" {
 
 #include "stdint.h"
 
-/* Error code */
-typedef enum {
-	JDR_OK = 0,	/* 0: Succeeded */
-	JDR_INTR,	/* 1: Interrupted by output function */
-	JDR_INP,	/* 2: Device error or wrong termination of input stream */
-	JDR_MEM1,	/* 3: Insufficient memory pool for the image */
-	JDR_MEM2,	/* 4: Insufficient stream input buffer */
-	JDR_PAR,	/* 5: Parameter error */
-	JDR_FMT1,	/* 6: Data format error (may be damaged data) */
-	JDR_FMT2,	/* 7: Right format but not supported */
-	JDR_FMT3	/* 8: Not supported JPEG standard */
-} JRESULT;
+/* Error codes (sin cambios) */
+typedef enum { JDR_OK = 0, JDR_INTR, JDR_INP, JDR_MEM1, JDR_MEM2, JDR_PAR, JDR_FMT1, JDR_FMT2, JDR_FMT3 } JRESULT;
 
+typedef struct { uint16_t left, right, top, bottom; } JRECT;
 
-
-/* Rectangular structure */
-typedef struct {
-	uint16_t left, right, top, bottom;
-} JRECT;
-
-
-
-/* Decompressor object structure */
 typedef struct JDEC JDEC;
 struct JDEC {
-	unsigned int dctr;			/* Number of bytes available in the input buffer */
-	uint8_t* dptr;				/* Current data read ptr */
-	uint8_t* inbuf;				/* Bit stream input buffer */
-	uint8_t dmsk;				/* Current bit in the current read byte */
-	uint8_t scale;				/* Output scaling ratio */
-	uint8_t msx, msy;			/* MCU size in unit of block (width, height) */
-	uint8_t qtid[3];			/* Quantization table ID of each component */
-	int16_t dcv[3];				/* Previous DC element of each component */
-	uint16_t nrst;				/* Restart inverval */
-	uint16_t width, height;		/* Size of the input image (pixel) */
-	uint8_t* huffbits[2][2];	/* Huffman bit distribution tables [id][dcac] */
-	uint16_t* huffcode[2][2];	/* Huffman code word tables [id][dcac] */
-	uint8_t* huffdata[2][2];	/* Huffman decoded data tables [id][dcac] */
-	int32_t* qttbl[4];			/* Dequantizer tables [id] */
-	void* workbuf;				/* Working buffer for IDCT and RGB output */
-	uint8_t* mcubuf;			/* Working buffer for the MCU */
-	void* pool;					/* Pointer to available memory pool */
-	unsigned int sz_pool;		/* Size of momory pool (bytes available) */
-	unsigned int (*infunc)(JDEC*, uint8_t*, unsigned int);	/* Pointer to jpeg stream input function */
-	void* device;				/* Pointer to I/O device identifiler for the session */
-	uint16_t x_offs;			/* x offset for slow method */
-	uint16_t y_offs;			/* y offset for slow method */
+	unsigned int dctr;
+	uint8_t* dptr;
+	uint8_t* inbuf;
+	uint8_t dmsk;
+	uint8_t scale;
+	uint8_t msx, msy;
+	uint8_t qtid[3];
+	int16_t dcv[3];
+	uint16_t nrst;
+	uint16_t width, height;
+	uint8_t* huffbits[2][2];
+	uint16_t* huffcode[2][2];
+	uint8_t* huffdata[2][2];
+	int32_t* qttbl[4];
+	void* workbuf;
+	uint8_t* mcubuf;
+	void* pool;
+	unsigned int sz_pool;
+	unsigned int (*infunc)(JDEC*, uint8_t*, unsigned int);
+	void* device;
+	uint16_t x_offs;
+	uint16_t y_offs;
 };
 
-
-/* TJpgDec API functions */
 JRESULT jd_prepare (JDEC* jd, unsigned int (*infunc)(JDEC*,uint8_t*,unsigned int), void* pool, unsigned int sz_pool, void* dev);
 JRESULT jd_decomp (JDEC* jd, int (*outfunc)(JDEC*,void*,JRECT*), uint8_t scale);
-
 
 #ifdef __cplusplus
 }
