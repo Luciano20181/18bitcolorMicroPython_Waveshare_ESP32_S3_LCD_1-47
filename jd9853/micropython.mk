3928c4db473b8f2b35b5becc4de821d7e1b532ae
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-06-01 12:14:39 -0300

    Update micropython.mk

--- jd9853/micropython.mk
+++ jd9853/micropython.mk
@@ -4,6 +4,6 @@ CFLAGS_USERMOD += -I$(JD9853_MOD_DIR)
 
 SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/, jd9853.c)
 SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/, mpfile.c)
-SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/jpg/, tjpgd565.c)
+SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/jpg/, tjpgd666.c)
 SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/png/, pngle.c)
 SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/png/, miniz.c)
